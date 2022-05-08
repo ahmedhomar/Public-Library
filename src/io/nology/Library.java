85e2be45a3d9216860a046af3eeb50a1d739d4e0
Ahmed Omar <ahmedomar@hey.com>
2022-05-08 20:17:09 +0100

    added fields and constructor to Library class

--- src/io/nology/Library.java
+++ src/io/nology/Library.java
@@ -1,4 +1,20 @@
 package io.nology;
 
+import java.util.HashMap;
+
 public class Library {
+    private final HashMap<Integer, Book> books;
+
+    // constructor
+    public Library() {
+        books = new HashMap<>();
+    }
+
+    // add a book to the library
+    public void registerBook(Book book) {
+        if (!books.containsKey(book.getNumber())) {
+            books.put(book.getNumber(), book);
+        }
+    }
+
 }
